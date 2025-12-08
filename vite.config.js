03119a299ace365e368ddcf2a1778039bd254342
natthpatphummek <natthpatphummek@gmail.com>
2025-12-08 17:21:39 +0700

    081268

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin'; // Import the Laravel plugin
-import tailwindcss from '@tailwindcss/vite'; // Optional, but helps if using the Vite-specific Tailwind plugin
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
-        laravel({ // Use the Laravel plugin
+        laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: [
-                'resources/views/**', // This line enables auto-reload on Blade file changes
+                'resources/views/**',
                 'routes/**',
             ],
         }),
         tailwindcss(),
-        // ... any other plugins
     ],
 });
